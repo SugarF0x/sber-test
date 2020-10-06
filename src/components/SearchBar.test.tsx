28e8197861100e3d9e909e1ebfdac19427cd2661
Sugar_F0x <sugarfox.dev@gmail.com>
2020-10-06 16:50:54 +0300

    :pill: Add buttons' disabled state test
    > Test buttons' state based on value of input fields

--- src/components/SearchBar.test.tsx
+++ src/components/SearchBar.test.tsx
@@ -54,7 +54,46 @@ describe('Search Component testing', () => {
    * Element interaction tests
    */
 
-  // test('Buttons disabled state', () => {
-  //   expect(false).toBe(false)
-  // })
+  test('Search button disabled state change on input', () => {
+    let description = container.find('input').at(0);
+    let location = container.find('input').at(1);
+    /*
+      i wanted to do a button variable
+      but for some reason it's state would not update on change
+      while the full path call works every time
+      ?question mark?
+     */
+    /**
+     * Initial state test
+     */
+    expect(container.find('button').at(0).prop('disabled')).toBe(true);
+
+    /**
+     * Stage change on Description input
+     */
+    description.simulate('change', { target: { value: 'js' } });
+    expect(container.find('button').at(0).prop('disabled')).toBe(false);
+    description.simulate('change', { target: { value: '' } });
+
+    /**
+     * State change on Location input
+     */
+    location.simulate('change', { target: { value: 'moscow' } });
+    expect(container.find('button').at(0).prop('disabled')).toBe(false);
+    location.simulate('change', { target: { value: '' } });
+
+    /**
+     * State change on both Description and Location input
+     */
+    description.simulate('change', { target: { value: 'js' } });
+    location.simulate('change', { target: { value: 'moscow' } });
+    expect(container.find('button').at(0).prop('disabled')).toBe(false);
+    description.simulate('change', { target: { value: '' } });
+    location.simulate('change', { target: { value: '' } });
+
+    /**
+     * State reversion to initial on input clear
+     */
+    expect(container.find('button').at(0).prop('disabled')).toBe(true);
+  })
 })
